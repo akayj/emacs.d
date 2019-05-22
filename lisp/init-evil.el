ee47469d4c159dfebdfa6ad04dea132fcbaa337c
akayj <askingyj@gmail.com>
2019-05-22 23:28:35 +0800

    添加evil leader key

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,22 +1,49 @@
+;; (use-package evil
+;;   :ensure t
+;;   :config
+;;   (evil-mode 1)
+;;   (evil-define-key 'normal go-mode-map "gd" 'godef-jump))
+
 (use-package evil
   :ensure t
-  :config
-  (evil-mode 1)
-  (evil-define-key 'normal go-mode-map "gd" 'godef-jump))
-
-;; evil-surround
-(use-package evil-surround
-  :ensure t
-  :config
-  (global-evil-surround-mode 1))
-
-;; evil-escape
-(use-package evil-escape
-  :ensure t
   :init
-  (setq-default evil-escape-key-sequence "jk")
+  (progn
+    (evil-mode t)
+    (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state))
+
   :config
-  (evil-escape-mode 1))
+  (progn
+    (use-package evil-leader
+      :ensure t
+      :init
+      (progn
+	(global-evil-leader-mode)
+	(setq evil-leader/in-all-states 1)
+	(evil-leader/set-leader "<SPC>")
+	(evil-leader/set-key "c" 'compile)
+	(evil-leader/set-key "m" 'magit-status)
+	(evil-leader/set-key "sb" 'ido-switch-buffer)
+	(evil-leader/set-key "sc" 'org-capture)
+	(evil-leader/set-key "k" 'ido-kill-buffer)))
+	(evil-leader/set-key "f" 'ido-find-file)))
+
+    ;; evil-surround
+    (use-package evil-surround
+      :ensure t
+      :config
+      (global-evil-surround-mode 1))
+
+    ;; evil-escape
+    (use-package evil-escape
+      :ensure t
+      :init
+      (setq-default evil-escape-key-sequence "jk")
+      :config
+      (evil-escape-mode 1))
+
+    )
+  )
+
 
 (provide 'init-evil)
 ;;; init-evil.el ends here
