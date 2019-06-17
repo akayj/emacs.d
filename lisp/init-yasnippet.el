f9bf3ee72b699455d0bf429a3f695b522097080d
akayj <askingyj@gmail.com>
2019-06-17 02:25:29 +0000

    auto create yasnippet directory

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -4,6 +4,8 @@
   (setq yas-snippet-dirs
 	'("~/.emacs.d/snippets"))
   :config
+  (if (not (f-exists-p "~/.emacs.d/snippets"))
+      (f-mkdir "~/.emacs.d/snippets"))
   (use-package yasnippet-snippets :ensure t)
 
   (require 'yasnippet)
