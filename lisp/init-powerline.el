b57853b4c4709578c6aa43a615a80e2e1c8c6a08
akayj <askingyj@gmail.com>
2020-04-29 03:19:00 +0800

    update

--- lisp/init-powerline.el
+++ lisp/init-powerline.el
@@ -29,6 +29,8 @@
   :config
   (setq doom-modeline-height 25)
   (setq doom-modeline-bar-width 3)
+  (set-face-attribute 'mode-line nil :family "Menlo" :height 125)
+  (set-face-attribute 'mode-line-inactive nil :family "Menlo" :height 125)
 
   (setq doom-modeline-icon (display-graphic-p))
   ;; (setq doom-modeline-major-mode-icon t)
@@ -38,8 +40,10 @@
   (setq doom-modeline-env-enable-python t)
   (setq doom-modeline-env-enable-go t)
 
+  (setq doom-modeline-major-mode-color-icon t)
+  (setq doom-modeline-lsp t)
   (setq doom-modeline-enable-word-count t)
-
+  (setq doom-modeline-project-detection 'project)
   (doom-modeline-def-modeline 'my-simple-line
     '(bar matches buffer-info remote-host buffer-position parrot selection-info)
     '(misc-info minor-modes input-method buffer-encoding major-mode process vcs checker))
