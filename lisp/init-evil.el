caf0979813dad537b1655b913bac5627e5511874
akayj <askingyj@gmail.com>
2020-04-30 23:31:34 +0800

    update

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -5,6 +5,7 @@
   :ensure t
   :init
   (progn
+    (global-evil-leader-mode)
     (evil-mode t)
     (evil-define-key 'normal go-mode-map "gd" 'godef-jump)
     (evil-define-key 'normal go-mode-map "gb" 'xref-pop-marker-stack)
@@ -16,7 +17,6 @@
       :ensure t
       :init
       (progn
-	(global-evil-leader-mode)
 	(setq evil-leader/in-all-states 1)
 	(evil-leader/set-leader "<SPC>")
 
--- lisp/init-tree.el
+++ lisp/init-tree.el
@@ -3,11 +3,33 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package all-the-icons :ensure t)
+(use-package all-the-icons :ensure t
+  ;; :config
+  ;; (all-the-icons-wicon "tornado")
+  ;; (insert (all-the-icons-icon-for-file "tsx"))
+  ;; (all-the-icons-icon-for-file "a.tsx")
+  ;; (all-the-icons-fileicon "tsx")
+  )
+
+(use-package find-file-in-project :ensure t)
+
+(defun neotree-ffip-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (ffip-project-root))
+	(file-name (buffer-file-name)))
+    (if project-dir
+	(progn
+	  (neotree-dir project-dir)
+	  (neotree-find file-name))
+      (message "Could not find git project root."))))
 
 (use-package neotree
   :ensure t
   :requires (all-the-icons evil)
+  :config
+  (evil-leader/set-key "g" 'neotree-ffip-project-dir)
+
   :hook
   (neotree-mode .
 	    (lambda ()
@@ -23,6 +45,7 @@
   :custom
   (neo-theme (if (display-graphic-p) 'icons 'arrow))
   )
+
 
 (provide 'init-tree)
 ;;; init-tree.el ends here
