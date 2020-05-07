5a84f3131cef3c4d3ef0c423702659671b068a1c
akayj <askingyj@gmail.com>
2020-05-07 17:40:45 +0800

    update org-mode settting

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,12 +6,20 @@
   :custom
   (org-html-validation-link nil)
   (org-todo-keywords
-   '((sequence "TODO" "WORKING" "HOLD" "|" "DONE")))
+   '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+     (sequence "WAITING(w@/!)" "HOLD(h@!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING")
+     ))
   (org-todo-keyword-faces
-   '(("TODO" . "blue")
-     ("WORKING" . "yellow")
-     ("HOLD" . "red")
-     ("DONE" . "green")))
+   '(("TODO" :foreground "red" :weight bold)
+     ("NEXT" :foreground "blue" :weight bold)
+     ("DONE" :foreground "forest green" :weight bold)
+     ("WAITING" :foreground "orange" :weight bold)
+     ("HOLD" :foreground "magenta" :weight bold)
+     ("CANCELLED" :foreground "forest green" :weight bold)
+     ("MEETING" :foreground "forest green" :weight bold)
+     ))
+
+  (org-directory "~/gits/org")
 
   :hook
   (org-mode . my-org-symbols)
