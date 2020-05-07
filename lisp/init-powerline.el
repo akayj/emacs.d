f65b774d88b870c71392a5fc97a991af9f06b9c3
akayj <askingyj@gmail.com>
2020-05-08 01:41:06 +0800

    替换自定义的modeline配置

--- lisp/init-powerline.el
+++ lisp/init-powerline.el
@@ -1,3 +1,68 @@
+;;; init-powerline.el -- custom my modeline
+
+;;; Commentary:
+
+;;; Code:
+
+;; (setq mode-line-format '("%e"))
+      ;; '(
+      ;;	))
+
+(defvar mode-line-cleaner-alist
+  `(
+    (auto-complete-mode . 945) ;; α
+    (yas-minor-mode . 9422)
+    (paredit-mode . nil)
+    (eldoc-mode . nil)
+    (abbrev-mode . nil)
+    (auto-revert-mode . nil)
+
+    (counsel-mode . nil)
+    (flycheck-mode . 9403)
+
+    ;; Major modes
+    (lisp-interaction-mode . 955) ;; λ
+    (hi-lock-mode . nil)
+    ;; (python-mode . "Py")
+    (python-mode . 128013)
+    (go-mode . 128063)
+
+    (emacs-lisp-mode . 8721)
+    (nxhtml-mode . "nx")
+
+    (helm-mode . 9405)
+    (company-mode . 9426)
+
+    (undo-tree-mode . nil)
+
+    (smartparens-mode . nil)
+    (whitespace-cleanup-mode . nil)
+
+    (evil-escape-mode . nil)
+    (evil-commentary-mode . nil)
+    )
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+
+(defun clean-mode-line ()
+  (interactive)
+  (dolist (cleaner mode-line-cleaner-alist)
+    (let* ((mode (car cleaner))
+	   (mode-val (cdr cleaner))
+	   (mode-str (if (numberp mode-val) (char-to-string mode-val) mode-val))
+	   (old-mode-str (cdr (assq mode minor-mode-alist))))
+      (when old-mode-str
+	(setcar old-mode-str (if mode-str (format " %s" mode-str) "")))
+      ;; major mode
+      (when (eq mode major-mode)
+	(setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
 ;; (use-package anzu
 ;;   :ensure t
 ;;   :config
@@ -21,40 +86,40 @@
 ;;   (spaceline-toggle-buffer-size-off)
 ;;   (spaceline-toggle-evil-state-on))
 
-(use-package doom-modeline
-  :ensure t
-  :hook
-  (after-init . doom-modeline-mode)
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :hook
+;;   (after-init . doom-modeline-mode)
 
-  :config
-  (setq doom-modeline-height 25)
-  (setq doom-modeline-bar-width 3)
-  (set-face-attribute 'mode-line nil :family "Menlo" :height 125)
-  (set-face-attribute 'mode-line-inactive nil :family "Menlo" :height 125)
+;;   :config
+;;   (setq doom-modeline-height 25)
+;;   (setq doom-modeline-bar-width 3)
+;;   (set-face-attribute 'mode-line nil :family "Menlo" :height 125)
+;;   (set-face-attribute 'mode-line-inactive nil :family "Menlo" :height 125)
 
-  (setq doom-modeline-icon (display-graphic-p))
-  ;; (setq doom-modeline-major-mode-icon t)
-  ;; (setq doom-modeline-buffer-state-icon t)
+;;   (setq doom-modeline-icon (display-graphic-p))
+;;   ;; (setq doom-modeline-major-mode-icon t)
+;;   ;; (setq doom-modeline-buffer-state-icon t)
 
-  ;; display the environment version.
-  (setq doom-modeline-env-enable-python t)
-  (setq doom-modeline-env-enable-go t)
+;;   ;; display the environment version.
+;;   (setq doom-modeline-env-enable-python t)
+;;   (setq doom-modeline-env-enable-go t)
 
-  (setq doom-modeline-major-mode-color-icon t)
-  (setq doom-modeline-lsp t)
-  (setq doom-modeline-enable-word-count t)
-  (setq doom-modeline-project-detection 'project)
-  (doom-modeline-def-modeline 'my-simple-line
-    '(bar matches buffer-info remote-host buffer-position parrot selection-info)
-    '(misc-info minor-modes input-method buffer-encoding major-mode process vcs checker))
+;;   (setq doom-modeline-major-mode-color-icon t)
+;;   (setq doom-modeline-lsp t)
+;;   (setq doom-modeline-enable-word-count t)
+;;   (setq doom-modeline-project-detection 'project)
+;;   (doom-modeline-def-modeline 'my-simple-line
+;;     '(bar matches buffer-info remote-host buffer-position parrot selection-info)
+;;     '(misc-info minor-modes input-method buffer-encoding major-mode process vcs checker))
 
-  (defun setup-custom-doom-modeline ()
-    (doom-modeline-set-modeline 'my-simple-line))
+;;   (defun setup-custom-doom-modeline ()
+;;     (doom-modeline-set-modeline 'my-simple-line))
 
-  (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
-  )
+;;   (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
+;;   )
 
-(use-package all-the-icons :ensure t)
+;; (use-package all-the-icons :ensure t)
 
 
 (provide 'init-powerline)
