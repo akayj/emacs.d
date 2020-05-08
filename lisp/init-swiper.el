c29b4ab7f382e5812db8fcdddefc5eb8ecdc5b71
akayj <askingyj@gmail.com>
2020-05-08 12:09:41 +0800

    Add keymap for swiper

--- lisp/init-swiper.el
+++ lisp/init-swiper.el
@@ -12,14 +12,18 @@
   :config
   (ivy-mode 1))
 
+(defvar swiper-evil-keymap nil "Swiper keyma for evil mode.")
+(setq swiper-evil-keymap (make-sparse-keymap))
+(progn
+  (define-key swiper-evil-keymap (kbd "a") 'counsel-ag)
+  (define-key swiper-evil-keymap (kbd "g") 'counsel-git)
+  )
+
 
 (use-package counsel
   :ensure t
   :config
-  ;; (evil-leader/set-key
-  ;;   "a" 'counsel-ag
-  ;;   "g" 'counsel-git
-  ;;   )
+  (evil-leader/set-key "v" swiper-evil-keymap)
   (counsel-mode 1)
   )
 
