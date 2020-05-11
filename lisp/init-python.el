a6215667fe7d833e0e0fc0e98fcd5321586042ec
akayj <askingyj@gmail.com>
2020-05-11 10:16:54 +0800

    add python indent setup

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,8 +1,22 @@
+;;; init-python.el -- support better python dev experience
+;;; Commentary:
+
+;;; Code:
+
 (use-package elpy
   :ensure t
   :defer t
   :init
-  (advice-add 'python-mode :before 'elpy-enable))
+  (advice-add 'python-mode :before 'elpy-enable)
+
+  :hook
+  (python .
+	  (lambda ()
+	    (setq tab-width 4)
+	    (setq indent-tabs-mode t)
+	    (setq python-indent-offset 4)
+	    ))
+  )
 
 (provide 'init-python)
 ;;; init-python.el ends here
