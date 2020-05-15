36ebd0e762051c6ea7affa612ef10192b990e2ea
akayj <askingyj@gmail.com>
2020-05-15 13:21:26 +0800

    使用 monokai-theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,13 +1,42 @@
 ;;; package -- Summary
 ;;; Code:
 ;;; Commentary:
-(use-package doom-themes
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   (load-theme 'doom-one t)
+
+;;   ;; Enable custom neotree theme
+;;   ;; (doom-themes-neotree-config)
+;;   )
+
+(use-package monokai-theme
   :ensure t
   :config
-  (load-theme 'doom-one t)
+  (load-theme 'monokai t)
 
-  ;; Enable custom neotree theme
-  ;; (doom-themes-neotree-config)
+  :custom
+  ;; foreground and background
+  ;; (monokai-foreground "#ABB2BF")
+  (monokai-foreground "white")
+  (monokai-background     "#282C34")
+  ;; highlights and comments
+  ;; (monokai-comments       "#F8F8F0")
+  (monokai-comments       "#999")
+  ;; (monokai-emphasis       "#282C34")
+  ;; (monokai-highlight      "#FFB269")
+  ;; (monokai-highlight-alt  "#66D9EF")
+  (monokai-highlight-line "#1B1D1E")
+  (monokai-line-number    "#F8F8F0")
+  ;; colours
+  (monokai-blue           "#61AFEF")
+  (monokai-cyan           "#56B6C2")
+  (monokai-green          "#98C379")
+  (monokai-gray           "#3E4451")
+  (monokai-violet         "#C678DD")
+  (monokai-red            "#E06C75")
+  (monokai-orange         "#D19A66")
+  (monokai-yellow         "#E5C07B")
   )
 
 (provide 'init-themes)
