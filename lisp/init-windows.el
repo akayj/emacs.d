98cb89cb698df6dfb31d6d0539ba25279505544d
akayj <askingyj@gmail.com>
2022-03-17 15:02:16 +0800

    set Windows to transparency.

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -5,5 +5,8 @@
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   )
 
+;; transpancy windows
+(set-frame-parameter (selected-frame) 'alpha (list 85 50))
+
 (provide 'init-windows)
 ;;; init-windows.el ends here
